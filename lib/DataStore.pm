27283a20cc0014f0e509a8e61d33b8576a96cc58
Isabel Polo <ipolo@etsetb.upc.edu>
2008-11-25 11:24:05 +0000

    Corretgit error a la condicio de load().
    git-svn-id: http://devel.cpl.upc.edu/recursos/svn/smeagol/trunk@111 4ab63a03-c2d3-4037-8559-9a21b594da05

--- lib/DataStore.pm
+++ lib/DataStore.pm
@@ -7,19 +7,18 @@ sub load {
     my $self = shift;
     my ($id) = @_;
     my $data = 0;
-    if(-e $id.'.db'){
+    if(-e "/tmp/".$id.'.db'){
         #$data = retrieve($id.'.db') or die;
         $data = require ("/tmp/".$id.'.db');
-	}
+    }
     return $data;
 }
 
 sub save {
     my $self = shift;
     my ($id,$data) = @_;
-	#nstore(\$data, $id.'.db') or die;
-    
-	open my $out, ">",  "/tmp/".$id.'.db' or die;
+    #nstore(\$data, $id.'.db') or die;
+    open my $out, ">",  "/tmp/".$id.'.db' or die;
     print $out Dumper($data);
     close $out;
 

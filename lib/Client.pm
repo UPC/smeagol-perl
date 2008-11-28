b71ba6ae4ad9d67b25f7abfe7984853c0e89c320
Isabel Polo <ipolo@etsetb.upc.edu>
2008-11-28 08:55:06 +0000

    Afegides funcions: 	create_resource 	retrieve_resource (Important: retorna un array amb l'estat i el contingut xml, o bé retorna només l'estat, segons s'hagi fet la crida) 	delete_resource 	create_booking 	retrieve_booking (Important: retorna un array amb l'estat i el contingut xml, o bé retorna només l'estat, segons s'hagi fet la crida)
    git-svn-id: http://devel.cpl.upc.edu/recursos/svn/smeagol/trunk@123 4ab63a03-c2d3-4037-8559-9a21b594da05

--- lib/Client.pm
+++ lib/Client.pm
@@ -3,6 +3,10 @@ package Client;
 # version -0.01 alfa-alfa-version 
 use strict;
 use warnings;
+use DateTime;
+use Resource;
+use Agenda;
+use Booking;
 
 use Carp;
 
@@ -10,38 +14,127 @@ my %COMMAND_VALID = map { $_ => 1 } qw( POST GET PUT DELETE );
 
 use LWP::UserAgent;
 
-sub _client_call {
-  my ($server,$url,$port,$command ) = @_;
-	$port ||=80;
-	$command ||= "GET";
-
-	croak "Error: Invalid Command '$command'" unless $COMMAND_VALID{$command};
-  # Create a user agent object
-  my $ua = LWP::UserAgent->new;
-  $ua->agent("SmeagolClient/0.1 ");
-
-  # Create a request
-  my $req = HTTP::Request->new($command => "http://$server:$port/$url");
-
-  # Pass request to the user agent and get a response back
-  my $res = $ua->request($req);
-
-  # Check the outcome of the response
-  if ($res->is_success) {
-      print $res->content;
-  }
-  else {
-      print $res->status_line, "\n";
-  }
-
-}
+my $port = 80;
+my $server = 'localhost';
 
 sub new {
     my $class = shift;
 
-    my $client = $class->SUPER::new(@_);
+    my $bless = {};
 
-    bless $client, $class;
+    bless $bless, $class;
+}
+
+sub _client_call {
+	my ($server,$url,$port,$command ) = @_;
+	$port ||=80;
+	$command ||= "GET";
+
+	croak "Error: Invalid Command '$command'" unless $COMMAND_VALID{$command};
+	# Create a user agent object
+	my $ua = LWP::UserAgent->new;
+	$ua->agent("SmeagolClient/0.1 ");
+
+	# Create a request
+	my $req = HTTP::Request->new($command => "http://$server:$port/$url");
+
+	# Pass request to the user agent and get a response back
+	my $res = $ua->request($req);
+
+	# Check the outcome of the response
+	if ($res->is_success) {
+		print $res->content;
+	}
+	else {
+      print $res->status_line, "\n";
+	}
+
+}
+
+###RESOURCE
+sub create_resource {
+	my $self = shift;
+	my ($id, $des, $gra) = @_;
+
+	my $req = HTTP::Request->new(POST => "http://$server:$port/resource");
+	$req->content_type('text/xml');
+	my $res_xml = "<resource>
+	    			<id>$id</id>
+	    			<description>$des</description>
+    				<granularity>$gra</granularity>
+					</resource>";
+    $req->content($res_xml);
+
+    my $ua  = LWP::UserAgent->new(); #Client
+    my $res = $ua->request($req); 
+
+	return $res->status_line;		
+}
+
+sub retrive_resource {
+	my $self = shift;
+	my ($id) = @_;
+
+	my $ua  = LWP::UserAgent->new();
+    my $res = $ua->get("$server:$port/resource/".$id);
+
+	return wantarray ? ($res->status_line,$res->content) : $res->status_line;
+
+}
+
+sub delete_resource {
+	my $self = shift;
+	my ($id) = @_;
+	my $req = HTTP::Request->new(POST => "http://$server:$port/resource/".$id);
+	$req->content_type('text/xml');
+
+    my $ua  = LWP::UserAgent->new();
+    my $res = $ua->request($req); 
+
+	return $res->status_line;
+}
+
+###BOOKING
+sub create_booking {
+	my $self = shift;
+	my (@from,@to) = @_;
+
+	my $req = HTTP::Request->new(POST => "http://$server:$port/booking");
+	$req->content_type('text/xml');
+	my $booking_xml = "<booking>
+						<from>
+							<year>$from[0]</year>
+							<month>$from[1]</month>
+							<day>$from[2]</day>
+							<hour>$from[3]</hour>
+							<minute>$from[4]</minute>
+							<second>$from[5]</second>
+						</from>
+						<to>
+							<year>$to[0]</year>
+							<month>$to[1]</month>
+							<day>$to[2]</day>
+							<hour>$to[3]</hour>
+							<minute>$to[4]</minute>
+							<second>$to[5]</second>
+						</to>
+					</booking>";
+    $req->content($booking_xml);
+
+    my $ua  = LWP::UserAgent->new(); #Client
+    my $res = $ua->request($req); 
+
+	return $res->status_line;
+}
+
+sub retrieve_booking {
+	my $self = shift;
+	my ($id) = @_;
+
+	my $ua  = LWP::UserAgent->new();
+    my $res = $ua->get("$server:$port/booking/".$id);
+
+	return wantarray ? ($res->status_line,$res->content) : $res->status_line;
 }
 
 1;

70b584d25d222d0b4b0510e214dcf7f77d611d72
Isabel Polo <ipolo@etsetb.upc.edu>
2008-12-02 09:48:57 +0000

    Encara falten comprovacions en general del funcionament. Substituides algunes comandes de les peticiones HTTP, per les que s'indiquen a la API. Afegides les funcions: 	update_resource 	list_bookings_resource 	create_booking_resource 	delete_booking 	update_booking
    git-svn-id: http://devel.cpl.upc.edu/recursos/svn/smeagol/trunk@129 4ab63a03-c2d3-4037-8559-9a21b594da05

--- lib/Client.pm
+++ lib/Client.pm
@@ -7,14 +7,12 @@ use DateTime;
 use Resource;
 use Agenda;
 use Booking;
-
+use LWP::UserAgent;
 use Carp;
 
 my %COMMAND_VALID = map { $_ => 1 } qw( POST GET PUT DELETE );
 
-use LWP::UserAgent;
-
-my $port   = 80;
+my $port   = 8000;
 my $server = 'localhost';
 
 sub new {
@@ -52,12 +50,12 @@ sub _client_call {
 
 }
 
-###RESOURCE
+###RESOURCE MAINTENANCE
 sub list_resources {
     my $self = shift;
 
     my $ua  = LWP::UserAgent->new();
-    my $res = $ua->get("$server:$port/resources");
+    my $res = $ua->get("http://$server:$port/resources");
 
     return
         wantarray ? ( $res->status_line, $res->content ) : $res->status_line;
@@ -66,28 +64,28 @@ sub list_resources {
 sub create_resource {
     my $self = shift;
     my ( $id, $des, $gra ) = @_;
-
-    my $req = HTTP::Request->new( POST => "http://$server:$port/resources" );
-    $req->content_type('text/xml');
-    my $res_xml = "<resource>
-	    			<id>$id</id>
-	    			<description>$des</description>
+	
+	my $res_xml = "<resource>
+					<id>$id</id>
+					<description>$des</description>
 					<granularity>$gra</granularity>
 					</resource>";
+    my $req = HTTP::Request->new( POST => "http://$server:$port/resource");
+    $req->content_type('text/xml');
     $req->content($res_xml);
 
-    my $ua  = LWP::UserAgent->new();    #Client
+    my $ua  = LWP::UserAgent->new();
     my $res = $ua->request($req);
 
     return $res->status_line;
 }
 
-sub retrive_resource {
+sub retrieve_resource {
     my $self = shift;
     my ($id) = @_;
 
     my $ua  = LWP::UserAgent->new();
-    my $res = $ua->get( "$server:$port/resource/" . $id );
+    my $res = $ua->get( "http://$server:$port/resource/$id" );
 
     return
         wantarray ? ( $res->status_line, $res->content ) : $res->status_line;
@@ -98,7 +96,7 @@ sub delete_resource {
     my $self = shift;
     my ($id) = @_;
     my $req  = HTTP::Request->new(
-        POST => "http://$server:$port/resource/" . $id );
+        DELETE => "http://$server:$port/resource/" . $id );
     $req->content_type('text/xml');
 
     my $ua  = LWP::UserAgent->new();
@@ -107,7 +105,72 @@ sub delete_resource {
     return $res->status_line;
 }
 
-###BOOKING
+sub update_resource {
+    my $self = shift;
+    my ( $id, $des, $gra ) = @_;
+	
+	my $res_xml = "<resource>
+					<id>$id</id>
+					<description>$des</description>
+					<granularity>$gra</granularity>
+					</resource>";
+    my $req = HTTP::Request->new( PUT => "http://$server:$port/resource/$id");
+    $req->content_type('text/xml');
+    $req->content($res_xml);
+
+    my $ua  = LWP::UserAgent->new();
+    my $res = $ua->request($req);
+
+    return $res->status_line;
+}
+
+
+###BOOKING MAINTENANCE
+
+sub list_bookings_resource {
+    my $self = shift;
+	my ($id) = @_;
+
+    my $ua  = LWP::UserAgent->new();
+    my $res = $ua->get("http://$server:$port/resource/$id/booking");
+
+    return
+        wantarray ? ( $res->status_line, $res->content ) : $res->status_line;
+}
+
+sub create_booking_resource {
+    my $self = shift;
+    my ( $id, @from, @to ) = @_;
+
+    my $req = HTTP::Request->new( POST => "http://$server:$port/resource/$id/booking" );
+    $req->content_type('text/xml');
+    my $booking_xml = "<booking>
+						<from>
+							<year>$from[0]</year>
+							<month>$from[1]</month>
+							<day>$from[2]</day>
+							<hour>$from[3]</hour>
+							<minute>$from[4]</minute>
+							<second>$from[5]</second>
+						</from>
+						<to>
+							<year>$to[0]</year>
+							<month>$to[1]</month>
+							<day>$to[2]</day>
+							<hour>$to[3]</hour>
+							<minute>$to[4]</minute>
+							<second>$to[5]</second>
+						</to>
+					</booking>";
+    $req->content($booking_xml);
+
+    my $ua  = LWP::UserAgent->new();    #Client
+    my $res = $ua->request($req);
+
+    return $res->status_line;
+}
+
+
 sub create_booking {
     my $self = shift;
     my ( @from, @to ) = @_;
@@ -145,10 +208,56 @@ sub retrieve_booking {
     my ($id) = @_;
 
     my $ua  = LWP::UserAgent->new();
-    my $res = $ua->get( "$server:$port/booking/" . $id );
+    my $res = $ua->get( "http://$server:$port/booking/$id");
 
     return
         wantarray ? ( $res->status_line, $res->content ) : $res->status_line;
 }
+
+sub delete_booking {
+    my $self = shift;
+    my ($id) = @_;
+    my $req  = HTTP::Request->new(
+        DELETE => "http://$server:$port/booking/$id" );
+    $req->content_type('text/xml');
+
+    my $ua  = LWP::UserAgent->new();
+    my $res = $ua->request($req);
+
+    return $res->status_line;
+}
+
+sub update_booking {
+    my $self = shift;
+    my ( $id, @from, @to ) = @_;
+	my $booking_xml = "<booking>
+						<from>
+							<year>$from[0]</year>
+							<month>$from[1]</month>
+							<day>$from[2]</day>
+							<hour>$from[3]</hour>
+							<minute>$from[4]</minute>
+							<second>$from[5]</second>
+						</from>
+						<to>
+							<year>$to[0]</year>
+							<month>$to[1]</month>
+							<day>$to[2]</day>
+							<hour>$to[3]</hour>
+							<minute>$to[4]</minute>
+							<second>$to[5]</second>
+						</to>
+					</booking>";
+	
+    my $req = HTTP::Request->new( PUT => "http://$server:$port/resource/$id");
+    $req->content_type('text/xml');
+    $req->content($booking_xml);
+
+    my $ua  = LWP::UserAgent->new();
+    my $res = $ua->request($req);
+
+    return $res->status_line;
+}
+
 
 1;

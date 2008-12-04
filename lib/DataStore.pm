c4e5864c2ee79965ac54ffc49f36971dcabada96
Angel Aguilera <angel.aguilera@gmail.com>
2008-12-04 10:16:57 +0000

    afegit exists(), list() i init_path()
    git-svn-id: http://devel.cpl.upc.edu/recursos/svn/smeagol/trunk@135 4ab63a03-c2d3-4037-8559-9a21b594da05

--- lib/DataStore.pm
+++ lib/DataStore.pm
@@ -3,14 +3,16 @@ use Data::Dumper;
 
 #use Storable qw(nstore retrieve);
 
+my $db_path = "/tmp/";
+
 sub load {
     my $self = shift;
     my ($id) = @_;
-    my $data = 0;
-    if ( -e "/tmp/" . $id . '.db' ) {
+    my $data;
+    if ( -e $db_path . $id . '.db' ) {
 
         #$data = retrieve($id.'.db') or die;
-        $data = require( "/tmp/" . $id . '.db' );
+        $data = require( $db_path . $id . '.db' );
     }
     return $data;
 }
@@ -20,10 +22,34 @@ sub save {
     my ( $id, $data ) = @_;
 
     #nstore(\$data, $id.'.db') or die;
-    open my $out, ">", "/tmp/" . $id . '.db' or die;
+    open my $out, ">", $db_path . $id . '.db' or die;
     print $out Dumper($data);
     close $out;
 
+}
+
+sub exists {
+    my $self = shift;
+    my ($id) = @_;
+    if ( -e $db_path . $id . '.db' ) {
+		return 1;
+    }
+	return 0;
+}
+
+sub list_id {
+	my $self = shift;
+	my @list;
+	my @files = <$db_path.*>;
+	foreach (@files) {
+		push @list, $_;
+	}
+	return @list;
+}
+
+sub init_path {
+	my $self = shift;
+	$db_path = @_;
 }
 
 1;

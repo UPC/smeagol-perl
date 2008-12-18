24c30b12016af6ebe250b57f05206dfc1bf3aad2
Angel Aguilera <angel.aguilera@gmail.com>
2008-12-18 10:06:33 +0000

    Actualitzat el missatge de llicència del server.pl perquè mostri la Affero en comptes de la GPLv3.
    git-svn-id: http://devel.cpl.upc.edu/recursos/svn/smeagol/trunk@157 4ab63a03-c2d3-4037-8559-9a21b594da05

--- server.pl
+++ server.pl
@@ -8,28 +8,35 @@ use Server;
 use DataStore;
 
 my $version   = "Smeagol server v0.1";
-my $copyright = "Copyright (C) 2008 Universitat Politècnica de Catalunya";
+my $copyright = <<END;
+Copyright (C) 2008  Angel Aguilera <angel.aguilera\@upc.edu>
+Copyright (C) 2008  Eulàlia Formentí <eulalia.formenti\@upc.edu>
+Copyright (C) 2008  Francesc Guasch <frankie\@etsetb.upc.edu>
+Copyright (C) 2008  Francisco Morillas <fmorillas\@etsetb.upc.edu>
+Copyright (C) 2008  Alex Muntada <alexm\@alexm.org>
+Copyright (C) 2008  Isabel Polo <ipolo\@etsetb.upc.edu>
+Copyright (C) 2008  Sebastià Vila <sebas\@lsi.upc.edu>
+END
 
-####################################################
-# License messages (using the GPLv3 as an example) #
-####################################################
+#####################################
+# License messages (GNU Affero GPL) #
+#####################################
 
 # Long license message
 my $license_full = <<END;
 $version
 $copyright
-
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU General Public License
+You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 END
 
@@ -37,9 +44,9 @@ END
 my $license_short = <<END;
 $version
 $copyright
-
 This is free software; see the source for copying conditions.  There is NO
 warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Affero General Public License for more details.
 END
 
 # command-line options (with default values)

3e9a869197125e7dbec1d4b40fd7de39e1884467
Alex Muntada <alexm@alexm.org>
2009-04-16 15:06:42 +0000

    * Added class `Resource::List`.  * Removed method `Resource::list_id`.  * Updated test for empty resource list.  * Added `lib/Resource/List.pm` to MANIFEST.  * Closes: #108
    git-svn-id: http://devel.cpl.upc.edu/recursos/svn/smeagol/branches/ticket108_r476@479 4ab63a03-c2d3-4037-8559-9a21b594da05

---
+++ lib/Resource/List.pm
@@ -0,0 +1,88 @@
+package Resource::List;
+
+use strict;
+use warnings;
+
+use DataStore;
+use Resource;
+use XML::LibXML;
+use XML;
+use Carp;
+
+use overload q{""} => \&__str__;
+
+sub new {
+    my $class = shift;
+
+    my $obj = [];
+
+    foreach my $id ( DataStore->list_id ) {
+        my $r = Resource->load($id);
+        push @$obj, $r if defined $r;
+    }
+
+    bless $obj, $class;
+    return $obj;
+}
+
+sub __str__ {
+    my $self = shift;
+    my ( $url, $isRootNode ) = @_;
+
+    my $xmlText = "<resources>";
+    for my $slot (@$self) {
+        $xmlText .= $slot->to_xml("");
+    }
+    $xmlText .= "</resources>";
+
+    return $xmlText
+        unless defined $url;
+
+    my $xmlDoc = eval { XML->new($xmlText) };
+    croak $@ if $@;
+
+    $xmlDoc->addXLink( "resources", $url );
+    if ($isRootNode) {
+        $xmlDoc->addPreamble("resources");
+        return "$xmlDoc";
+    }
+    else {
+
+        # Take the first node and skip processing instructions
+        my $node = $xmlDoc->doc->getElementsByTagName("resources")->[0];
+        return $node->toString;
+    }
+}
+
+# DEPRECATED
+sub to_xml {
+    return shift->__str__(@_);
+}
+
+sub from_xml {
+    my $class = shift;
+    my ($xml) = @_;
+
+    # validate XML string against the DTD
+    my $dtd = XML::LibXML::Dtd->new( "CPL UPC//Resources DTD v0.03",
+        "dtd/resoure-list.dtd" );
+
+    my $dom = eval { XML::LibXML->new->parse_string($xml) };
+
+    if ( ( !defined $dom ) || !$dom->is_valid($dtd) ) {
+
+        # validation failed
+        return;
+    }
+
+    my $obj = [];
+    bless $obj, $class;
+
+    for my $domResource ( $dom->getElementsByTagName('resource') ) {
+        push @$obj, Resouce->from_xml( $domResource->toString(0) );
+    }
+
+    return $obj;
+}
+
+1;

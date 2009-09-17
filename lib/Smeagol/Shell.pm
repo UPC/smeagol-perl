69de0e1514e5896bedafb9a042e3282fc9a815ab
Isabel Polo <ipolo@etsetb.upc.edu>
2009-09-17 09:29:01 +0000

    Afegides funcions de crear i llistar recursos, encara que no mostra els resultat amb les respectives descripcions.
    git-svn-id: http://devel.cpl.upc.edu/recursos/svn/smeagol/branches/r701_t93@704 4ab63a03-c2d3-4037-8559-9a21b594da05

--- lib/Smeagol/Shell.pm
+++ lib/Smeagol/Shell.pm
@@ -6,19 +6,44 @@ use warnings;
 use Smeagol::Client;
 
 use base qw(Term::Shell);
+our $client;
+our $resource;
 
-sub run_foo {
-  print "foo\n";
+sub init {
+  my $self = shift;
+  my $serverPort = 8000;
+  my $server     = "http://localhost:$serverPort";
+  $client = Smeagol::Client->new($server);
 }
 
 sub run_crea_recurs{
   my $self = shift;
   my ($desc) = @_;
-  print "crear recurs amb les dades següents:\n";
-  print "            descripcio: $desc\n";
+  print "ERROR: Cal incloure una descripcio\n" if (!defined $desc);
+  my $resource = $client->createResource($desc);
+  print " $resource\n";
 }
+
 sub smry_crea_recurs { "Crea un recurs senzill sense reserves inicials" }
 sub help_crea_recurs { "La descripció és obligatoria, la informació adicional i les etiquetes són opcionals\n"; }
+sub comp_crea_recurs {
+  my $self = shift;
+}
+
+sub run_llista_recursos{
+  my $self = shift;
+  my @res = $client->listResources();
+  foreach(@res){
+    print " $_\n";
+  }
+}
+
+sub smry_llista_recursos { "Llista tots els recursos existents" }
+sub help_llista_recursos { "()\n"; }
+sub comp_llista_recursos {
+  my $self = shift;
+}
+
 
 sub msg_unknown_cmd {
   my $self = shift;  

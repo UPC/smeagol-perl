055f832f2465f6ef732b5633b86be82c20c92a13
Jordi Amorós <jamoros@etsetb.upc.edu>
2010-07-20 17:09:42 +0000

    Afegida vista en HTML més els mètodes PUT i DELETE fets en jQuery
    Ara cal modificar els controladors per a que triïn entre una o altra vista segons convingui
    i ja estarà tot.
    
    
    
    git-svn-id: http://devel.cpl.upc.edu/recursos/svn/smeagol/branches/tiquet_225@1162 4ab63a03-c2d3-4037-8559-9a21b594da05

---
+++ lib/V2/Server/View/HTML.pm
@@ -0,0 +1,45 @@
+package V2::Server::View::HTML;
+
+use strict;
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config({
+    INCLUDE_PATH => [
+        V2::Server->path_to( 'root', 'src' ),
+        V2::Server->path_to( 'root', 'lib' ),
+	V2::Server->path_to( 'root', 'templates' ),
+    ],
+    PRE_PROCESS  => 'config/main',
+    WRAPPER      => 'site/wrapper',
+    ERROR        => 'error.tt2',
+    TIMER        => 0,
+    render_die   => 1,
+    CONTENT_TYPE => 'text/html',
+
+});
+
+=head1 NAME
+
+V2::Server::View::HTML - Catalyst TTSite View
+
+=head1 SYNOPSIS
+
+See L<V2::Server>
+
+=head1 DESCRIPTION
+
+Catalyst TTSite View.
+
+=head1 AUTHOR
+
+Jordi Amorós Andreu,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
+

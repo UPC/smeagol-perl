5abde974f93ed4fd16a2de3883bc457f69a9e124
H. Daniel Cesario <maneta@gmail.com>
2011-11-11 10:22:12 +0000

    Redefinido los limites de los parametros del id
    git-svn-id: http://devel.cpl.upc.edu/recursos/svn/smeagol/branches/tiquet_341@1621 4ab63a03-c2d3-4037-8559-9a21b594da05

--- lib/V2/Server/Obj/Tag.pm
+++ lib/V2/Server/Obj/Tag.pm
@@ -4,8 +4,8 @@ use Moose;
 use Moose::Util::TypeConstraints;
 use MooseX::Params::Validate;
 
-subtype 'ValidDescTag' => as 'Str' => where { length($_) <= 8000 } => message { 'The description parameter is not valid' };
-subtype 'ValidIdTag' => as 'Str' => where { length($_) <= 64 } => message { 'The tag identifier is not valid' };
+subtype 'ValidDescTag' => as 'Str' => where { length($_) <= 256 } => message { 'The description parameter is not valid' };
+subtype 'ValidIdTag' => as 'Str' => where { length($_) > 0 and length($_) <= 64 } => message { 'The tag identifier is not valid' };
 
 has 'id'          => ( isa => 'ValidIdTag',       is => 'ro' );
 has 'description' => ( isa => 'ValidDescTag',     is => 'rw' );

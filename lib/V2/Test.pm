37cc6729ae424e4c9bbe84324cee5a3958cd8807
Alex Muntada <alexm@alexm.org>
2011-12-18 23:23:18 +0000

    merge branches/tiquet_330_booking@1735
    git-svn-id: http://devel.cpl.upc.edu/recursos/svn/smeagol/trunk@1736 4ab63a03-c2d3-4037-8559-9a21b594da05

---
+++ lib/V2/Test.pm
@@ -0,0 +1,128 @@
+package V2::Test;
+
+use Moose;
+use Catalyst::Test 'V2::Server';
+
+use Test::More;
+
+use Carp qw( croak       );
+use JSON qw( decode_json );
+
+use List::Compare         ();
+use HTTP::Request::Common ();
+
+BEGIN {
+    my @op_list = qw( GET POST DELETE );
+
+    for my $op (@op_list) {
+        no strict 'refs';
+
+        *{ "HTTP_$op" } = \&{ "HTTP::Request::Common::$op" };
+    }
+}
+
+# FIXME (CPAN #65843)
+sub HTTP_PUT {
+    my $req = HTTP_POST(@_);
+    $req->method('PUT');
+
+    return $req;
+}
+
+has 'uri' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+sub _list_of_id {
+    return map { $_->{'id'} } @_;
+}
+
+sub GET {
+    my $self = shift;
+    my ($id) = @_;
+
+    my $uri  = $self->uri;
+    $uri    .= "/$id" if @_;
+
+    my $json;
+    subtest "GET $uri request" => sub {
+        my $res  = request HTTP_GET($uri);
+
+        ok( $res->is_success, "GET $uri successful" );
+
+        $json = decode_json( $res->content );
+
+        ok( ref($json) eq 'ARRAY' || ref($json) eq 'HASH', "GET $uri content is json" );
+
+        done_testing();
+    };
+
+    return @_ ? $json : _list_of_id(@$json);
+}
+
+sub POST {
+    my $self = shift;
+    my @args = @_;
+
+    croak "args needed" unless @args;
+
+    my $uri = $self->uri;
+
+    my @new_ids;
+    subtest "POST $uri request" => sub {
+        my @before = $self->GET();
+        my $res    = request HTTP_POST( $uri, @args );
+
+        ok( $res->is_success, "POST $uri successful" );
+
+        my @after = $self->GET();
+        @new_ids  = List::Compare->new( \@before, \@after )->get_complement;
+
+        ok( @new_ids == 1, "POST $uri created @new_ids" );
+
+        done_testing();
+    };
+
+    return $new_ids[0];
+}
+
+sub PUT {
+    my $self = shift;
+    my $id   = shift // croak "id needed";
+    my $uri  = $self->uri . "/$id";
+    my @args = @_;
+
+    croak "args needed" unless @args;
+
+    subtest "PUT $uri request" => sub {
+        my $res = request HTTP_PUT( $uri, @args );
+
+        ok( $res->is_success, "PUT $uri successful" );
+        
+        done_testing();
+    };
+
+    return;
+}
+
+sub DELETE {
+    my $self = shift;
+    my $id   = shift // croak "id needed";
+
+    my $uri  = $self->uri;
+    $uri    .= "/$id";
+
+    subtest "DELETE $uri request" => sub {
+        my $res  = request HTTP_DELETE($uri);
+
+        ok( $res->is_success, "DELETE $uri successful" );
+        
+        done_testing();
+    };
+
+    return;
+}
+
+1;
---
+++ t/api_Booking_redux.t
@@ -0,0 +1,91 @@
+#!perl
+
+use strict;
+use warnings;
+
+BEGIN {
+    require 't/TestingDB.pl';
+}
+
+use V2::Test;
+use Test::More;
+use utf8::all;
+
+my $res_id = V2::Test->new( uri => '/resource' )->POST([
+    description => 'resource_description',
+    info        => 'resource_info',
+]);
+
+my $ev_id = V2::Test->new( uri => '/event' )->POST([
+    description => 'event_description',
+    info        => 'event_info',
+    starts      => '2011-12-08T00:00:00',
+    ends        => '2011-12-08T23:59:00',
+    tags        => '',
+    bookings    => '',
+]);
+
+my $b = V2::Test->new( uri => '/booking' );
+
+my @bookings = $b->GET();
+
+is_deeply( \@bookings, [], 'get empty list of bookings' );
+
+my %book1 = (
+    info         => 'info1',
+    dtstart      => '2011-12-08T00:00:00',
+    dtend        => '2011-12-08T23:59:00',
+    id_resource  => $res_id,
+    id_event     => $ev_id,
+    freq         => 'daily',
+    interval     => 1,
+#    by_minute    => '',
+#    until        => '',
+#    by_hour      => '',
+#    by_day       => '',
+#    by_month     => '',
+#    by_day_month => '',
+#    exception    => '',
+
+
+    # FIXME (bug #355)
+    #
+    # Cal passar obligatòriament els tags '''com string'''.
+    # smeagol/branches/tiquet_330_booking/lib/V2/Server/Controller/Booking.pm#L356
+    #
+    tags => [],
+);
+
+my $id  = $b->POST([ %book1 ]);
+my $out = $b->GET($id);
+delete $out->{'duration'};
+delete $out->{'frequency'};
+delete $out->{'until'};
+
+is_deeply( $out, { %book1, id => $id }, "create booking1" );
+
+@bookings = $b->GET();
+
+is_deeply( \@bookings, [ $id ], 'list of 1 booking' );
+
+$book1{'info'} = 'edited-info';
+$b->PUT( $id, [ %book1 ] );
+
+@bookings = $b->GET();
+
+is_deeply( \@bookings, [ $id ], 'still list of 1 booking' );
+
+$out = $b->GET($id);
+delete $out->{'duration'};
+delete $out->{'frequency'};
+delete $out->{'until'};
+
+is_deeply( $out, { %book1, id => $id }, "edit booking1" );
+
+$b->DELETE($id);
+
+@bookings = $b->GET();
+
+is_deeply( \@bookings, [], 'delete gets empty list back' );
+
+done_testing();
